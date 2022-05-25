0cbd0d6237b416aca0e346ff3ea8febe72d8e14b
fvjupiter <schoof.frederik@gmail.com>
2022-05-25 19:30:32 +0200

    smooth scroll support

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import smoothscroll from 'smoothscroll-polyfill';
 
 function MyApp({ Component, pageProps }) {
   const screenRef = useRef()
   const [screen, setscreen] = useState({ height: 0, width: 0 })
   useEffect(() => {
+    smoothscroll.polyfill();
     const observer = new ResizeObserver(entries => {
         setscreen({ height: entries[0].contentRect.height, width: entries[0].contentRect.width })
     })
